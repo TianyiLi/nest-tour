f6123963ce724a61ff9f1ebfe5c5102ed716552c
Paul Lee <e0991100238@gmail.com>
2020-11-05 18:19:53 +0800

    feat: add event source example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,8 +10,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('cats')
-  getCat() {
-    return 'control in app'
-  }
 }
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,59 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
+import { Observable, fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { EventEmitter } from 'events';
+
+const e = new EventEmitter();
+interface messageQStruct {
+  id: number;
+  type: 'message' | 'pending';
+  content?: string;
+  clientId: string;
+}
 
 @Controller('cats')
 export class CatsController {
+  private messageQ: messageQStruct[] = [];
+  private e = new EventEmitter();
+
   @Get('')
-  findAll(): string {
+  async findAll() {
+    e.emit('message', 'test');
     return 'This action return all cats';
   }
 
   @Get('note')
   note(): string {
-    return 'note'
+    return 'note';
+  }
+
+  @Get('getSSe')
+  getHello() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    
+    <body>
+      <script>
+        const e = new EventSource('/cats/sse')
+        e.onmessage = ({data}) => console.log(data)
+      </script>
+    </body>
+    
+    </html>`;
+  }
+
+  @Sse('sse')
+  sse(): Observable<{ data: any }> {
+    return fromEvent<string>(e, 'message').pipe(
+      map(data => (console.log(data), { data: { message: data } })),
+    );
   }
 }
